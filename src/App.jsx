3e66191ca8e45716f2ae65ef6e37918deee59b39
Yuno <59208657+talongkhanh@users.noreply.github.com>
2020-11-02 20:19:38 +0700

    useState remove todo item from todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import "./App.scss";
-import ColorBox from "./components/ColorBox/ColorBox";
+import React, { useState } from 'react';
+import './App.scss';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'khanh dep trai' },
+    { id: 2, title: 'react hooks basic' },
+    { id: 3, title: 'todo list item' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+
+  }
   return (
     <div className="app">
-      <h1>Hello, World!</h1>
-      <ColorBox />
+      <h1>Todo - List</h1>
+      <TodoList
+        todos={todoList}
+        onTodoClick={handleTodoClick}
+      />
     </div>
   );
 }
